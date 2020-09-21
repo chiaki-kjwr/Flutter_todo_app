5b696b6b57e5b03121e54aaa9ca4b5f037c078d9
chiaki-kjwr <chiakikajiwara@gmail.com>
2020-09-21 20:37:48 +0900

    ボタンの追加

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,34 @@ void main() => runApp(TodoApp());
 
 class TodoApp extends StatelessWidget {
   @override
-  Widget build(BuileContext, context) {
+  Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: Scaffold(
-        appBar: AppBar(),
-        body: Container(),
+        appBar: AppBar(
+          centerTitle: false,
+          leading: Icon(Icons.videocam),
+          title: const Text(
+            'youtube_clone',
+          ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Icon(Icons.search),
+          ),
+          FlatButton(
+            child: Icon(Icons.more_vert),
+          ),
+        ],
+        body: Container(
+            child: Column(
+          children: <Widget>[
+            Image,
+            network(''),
+          ],
+        )),
       ),
     );
   }
