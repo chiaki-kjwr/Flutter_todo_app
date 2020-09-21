6cf1991227eec6d900a784bf8695974dc8b026ce
chiaki-kjwr <chiakikajiwara@gmail.com>
2020-09-21 21:30:49 +0900

    providerのインストール

--- lib/main.dart
+++ lib/main.dart
@@ -5,34 +5,12 @@ void main() => runApp(TodoApp());
 class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final String kboyText = 'kboyじゃないよ';
     return MaterialApp(
       theme: ThemeData(
         primaryColor: Colors.white,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          leading: Icon(Icons.videocam),
-          title: const Text(
-            'youtube_clone',
-          ),
-        ),
-        actions: <Widget>[
-          FlatButton(
-            child: Icon(Icons.search),
-          ),
-          FlatButton(
-            child: Icon(Icons.more_vert),
-          ),
-        ],
-        body: Container(
-            child: Column(
-          children: <Widget>[
-            Image,
-            network(''),
-          ],
-        )),
-      ),
+      home: Scaffold(),
     );
   }
 }
