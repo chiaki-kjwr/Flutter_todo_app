656583804b3a7e0bf1afde2c196cc4b3fb93bb9d
chiaki-kjwr <chiakikajiwara@gmail.com>
2020-09-21 18:38:06 +0900

    scaffoldの画面設計の変更

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(TodoApp());
 
-//ステートレス
 class TodoApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(),
-      body:Container(
-        color: Colors.red,
-        child: Text('Todo')
+  Widget build(BuileContext, context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Container(),
       ),
-      RaisedButton(
-        child: Text('つぎへ'),
-        onPressed: () {
-        Navigator.push (
-            context,
-            MaterialPageRoute(builder: (context) => NextPage('値を渡すよ'),)
-          ),
-        },
-      ),
-    initialRoute: '/',
-    routes {
-
-    }
-
-    )
-  };
+    );
+  }
 }
-
-//ステートフル
